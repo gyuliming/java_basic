207cc223cbb27ce157a131683a75bf6eca699012
GYULIM <songkl987naver.com>
2025-07-24 12:01:36 +0900

    day02

---
+++ src/day02/VariableEx01.java
@@ -0,0 +1,18 @@
+package day02;
+
+public class VariableEx01 {
+    public static void main(String[] args) {
+        // 1. 정수형 변수 value를 선언
+        int value;
+
+        // 2. 변수 value에 20 할당
+        value = 20;
+
+        // 3. 변수 result에 value 변수에 30을 더한 후 저장
+        int result;
+        result = value + 30;
+
+        // 4. 변수 result의 값을 콘솔에 출력
+        System.out.println(result);
+    }
+}
