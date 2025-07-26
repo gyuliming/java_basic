19f119a3855588e653a9af8fde0f30ba9608479c
GYULIM <songkl987naver.com>
2025-07-26 23:40:53 +0900

    반복제어문1_자가진단1

---
+++ src/jungol/반복제어문1/반복제어문1_자가진단1.java
@@ -0,0 +1,10 @@
+package jungol.반복제어문1;
+
+public class 반복제어문1_자가진단1 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 15; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
