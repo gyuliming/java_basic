07927b0bf999c1bfbd27fb41bed8ab138544362b
gyuliming <songkl987@naver.com>
2025-08-06 17:55:05 +0900

    생성자 및 상속 과제

---
+++ src/javabasic_02/day11/constructorAssignment/Rectangle2.java
@@ -0,0 +1,37 @@
+package javabasic_02.day11.constructorAssignment;
+
+public class Rectangle2 {
+    private int width;
+    private int length;
+    private String color;
+
+    public void setWidth(int width) {
+        this.width = width;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    // 면적
+    public int area() {
+        return width * length;
+    }
+
+    // 둘레
+    public int perimeter() {
+        return (width * 2) + (length * 2);
+    }
+}
